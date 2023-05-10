990411fc100b6ff235af10cf98501b5386d8b353
khd6194 <kdh6194@naver.com>
2023-05-10 22:01:02 +0900

    코딩테스트 8 - 4

--- src/main/java/GrmAns/grm32Ans.java
+++ src/main/java/GrmAns/grm32Ans.java
@@ -5,11 +5,9 @@ public class grm32Ans {
     static int[] num = {2,7,11,15};
     static int target = 9;
     public static int[] twoSum (int[] num,int target) {
-        int i = 0;
-        int j = 0;
 
-        for (i =0; i< num.length;i++){
-            for (j = 0; j < num.length; j++) {
+        for (int i =0; i< num.length;i++){
+            for (int j = 0; j < num.length; j++) {
                 if(num[i] + num[j] == target){
                     int min = num[j];
                     int max = num[i];
@@ -17,8 +15,8 @@ public class grm32Ans {
                     int nindex = j;
                     int xindex = i;
 
-                    System.out.println(num[i]);
                     System.out.println(num[j]);
+                    System.out.println(num[i]);
                     System.out.println("더해서 타겟에 나오는 첫번째값"+min);
                     System.out.println("더해서 타겟에 나오는 두번째값"+max);
                     System.out.println("더해서 타겟에 나오는 첫번째값의 인덱스"+nindex);
