a204f452a4b17015e00c5eb498a15fb00c015c79
khd6194 <kdh6194@naver.com>
2023-05-10 21:31:41 +0900

    코딩테스트 8 - 2

--- src/main/java/GrmAns/grm33Ans.java
+++ src/main/java/GrmAns/grm33Ans.java
@@ -4,35 +4,21 @@ import java.util.Scanner;
 
 public class grm33Ans {
     public static void main(String[] args) {
-        // 더하기 마이너스
-        //숫자 하나를 입력받고, 그 숫자만큼 +와 -를 반복해서 출력하는 구문
-        System.out.println("숫자를 입력하세요.");
+        //주어진 정수를 각 자릿수별로 더하는 방법
+        Scanner sc = new Scanner(System.in);
 
-        int number;
+        // 정수 입력받기
+        System.out.print("숫자를 입력하세요: ");
+        int num = sc.nextInt();
 
-        Scanner scan = new Scanner(System.in);
-
-        number = scan.nextInt();
-
-
-        //반복문을 사용해서 입력한 숫자만큼 출력되게 함
-        for(int i = 1; i <= number; i++) {
-
-            //1보다 크고, 2로 나누어서 나머지가 1이되는 i일때는 -가 앞에 출력 (즉, 홀수일때는 -가 앞에 출력)
-            if(i > 1 && i % 2 == 1) {
-
-                System.out.print("-");
-
-
-                //2로 나누어서 나머지가 0이되는 i일때는 +가 앞에 출력 (즉, 짝수일때는 +가 앞에 출력)
-            }else if(i % 2 == 0){
-
-                System.out.print("+");
-
-            }
-
-            System.out.printf("%d"+" ",i);
-
+        // 각 자릿수별로 더하기
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;  // 일의 자리 수 더하기
+            num /= 10;  // 일의 자리 수 제거
         }
+
+        // 결과 출력
+        System.out.println("각 자릿수의 합: " + sum);
     }
 }
